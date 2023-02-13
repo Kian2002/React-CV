f0c5352163e5d4888139dd5084e37f3149b2219b
Kian2002 <canadakurdistan@gmail.com>
2023-02-13 01:37:03 -0800

    Education component

--- src/components/Input/Education.jsx
+++ src/components/Input/Education.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "../styles/input.module.css";
 
 const Education = () => {
-  return <div>Education</div>;
+  return (
+    <div className={styles.wrapper}>
+      <h1>Education</h1>
+      <div className={styles["input-wrapper"]}>
+        <input type="text" placeholder="University Name" />
+        <input type="text" placeholder="City" />
+        <input type="text" placeholder="Degree" />
+        <input type="text" placeholder="Subject" />
+        <input type="text" placeholder="From" />
+        <input type="text" placeholder="To" />
+      </div>
+    </div>
+  );
 };
 
 export default Education;
