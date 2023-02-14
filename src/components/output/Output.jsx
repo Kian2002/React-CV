35d98d939e817b26443364e6059cfdaa5b2f82ae
Kian2002 <canadakurdistan@gmail.com>
2023-02-13 23:26:57 -0800

    output layout

--- src/components/output/Output.jsx
+++ src/components/output/Output.jsx
@@ -7,9 +7,24 @@ const Output = () => {
       <header className={styles.header}> </header>
       <div className={styles.main}>
         <div>
-          <div>Experience</div>
-          <div>Education</div>
+          <div>
+            <h1>Description</h1>
+            <p>lorem</p>
+          </div>
+          <div>
+            <h1>Experience</h1>
+            <span>From - To</span>
+            <span>Position-Company-City</span>
+          </div>
+          <div>
+            <h1>Education</h1>
+            <span>From - To</span>
+            <span>Name-City</span>
+            <span>Degree</span>
+            <span>Subject</span>
+          </div>
         </div>
+
         <div>PersonalInformation</div>
       </div>
     </div>
