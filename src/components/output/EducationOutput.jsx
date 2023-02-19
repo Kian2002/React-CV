6bb10a9abda87691583a0ca1121ff0c9eb14dc59
Kian2002 <canadakurdistan@gmail.com>
2023-02-18 23:13:48 -0800

    education output css setup

--- src/components/output/EducationOutput.jsx
+++ src/components/output/EducationOutput.jsx
@@ -3,19 +3,24 @@ import styles from "../styles/output.module.css";
 
 const EducationOutput = ({ educationInfo }) => {
   return (
-    <div className={styles["education-wrapper"]}>
-      <h3 className={styles["education-wrapper--h3"]}>Education</h3>
+    <div className={styles["wrapper__main"]}>
+      <h3 className={styles["wrapper__h3"]}>Education</h3>
       {educationInfo.map((item) => {
         return (
-          <div key={item.id} className={styles["education-wrapper--content"]}>
-            <div>
+          <div key={item.id} className={styles["wrapper__content"]}>
+            <div className={styles["wrapper__content__date"]}>
               {item.from} - {item.to}
             </div>
-            <div>
-              {item.universityName} {item.city}
+            <div className={styles["wrapper__content__main"]}>
+              <div>
+                <h4>
+                  {item.universityName}, {item.city}
+                </h4>
+
+                <span>Degree: {item.degree}</span>
+                <span>Subject: {item.subject}</span>
+              </div>
             </div>
-            <div>Degree: {item.degree}</div>
-            <div>Subject: {item.subject}</div>
           </div>
         );
       })}
