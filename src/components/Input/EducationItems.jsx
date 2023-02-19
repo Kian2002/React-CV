79c659daaaf602031a4e30805754798e5b70905a
Kian2002 <canadakurdistan@gmail.com>
2023-02-18 17:43:58 -0800

    add educationItem functionality

--- src/components/Input/EducationItems.jsx
+++ src/components/Input/EducationItems.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const EducationItems = ({ setEducationItem }) => {
+const EducationItems = ({ setEducationInfo, id }) => {
+  const [educationItems, setEducationItems] = useState([
+    {
+      id: id,
+      universityName: "",
+      city: "",
+      degree: "",
+      subject: "",
+      from: "",
+      to: "",
+    },
+  ]);
+
+  useEffect(() => {
+    setEducationInfo((prevEducationInfo) => {
+      const index = prevEducationInfo.findIndex((item) => {
+        item.id === id;
+      });
+
+      if (index !== -1) {
+        const updatedExperienceInfo = [...prevEducationInfo];
+        updatedExperienceInfo[index] = educationItems;
+        return updatedExperienceInfo;
+      } else {
+        return [...prevEducationInfo, educationItems];
+      }
+    });
+  }, [educationItems]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setEducationItems({
+      ...educationItems,
+      [name]: value,
+    });
+  };
   return (
     <>
-      <input type="text" name="universityName" placeholder="University name" />
+      <input
+        type="text"
+        name="universityName"
+        placeholder="University name"
+        onChange={handleChange}
+      />
       <input type="text" name="city" placeholder="City" />
       <input type="text" name="degree" placeholder="Degree" />
       <input type="text" name="subject" placeholder="Subject" />
