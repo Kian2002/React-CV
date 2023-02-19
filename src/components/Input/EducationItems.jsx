3182f9fb11ff072ebf79ab75190d28da16632496
Kian2002 <canadakurdistan@gmail.com>
2023-02-18 18:50:41 -0800

    add education Items setup

--- src/components/Input/EducationItems.jsx
+++ src/components/Input/EducationItems.jsx
@@ -1,28 +1,23 @@
 import React, { useEffect, useState } from "react";
 
 const EducationItems = ({ setEducationInfo, id }) => {
-  const [educationItems, setEducationItems] = useState([
-    {
-      id: id,
-      universityName: "",
-      city: "",
-      degree: "",
-      subject: "",
-      from: "",
-      to: "",
-    },
-  ]);
+  const [educationItems, setEducationItems] = useState({
+    id: id,
+    universityName: "",
+    city: "",
+    degree: "",
+    subject: "",
+    from: "",
+    to: "",
+  });
 
   useEffect(() => {
     setEducationInfo((prevEducationInfo) => {
-      const index = prevEducationInfo.findIndex((item) => {
-        item.id === id;
-      });
-
+      const index = prevEducationInfo.findIndex((item) => item.id === id);
       if (index !== -1) {
-        const updatedExperienceInfo = [...prevEducationInfo];
-        updatedExperienceInfo[index] = educationItems;
-        return updatedExperienceInfo;
+        const updatedEducationInfo = [...prevEducationInfo];
+        updatedEducationInfo[index] = educationItems;
+        return updatedEducationInfo;
       } else {
         return [...prevEducationInfo, educationItems];
       }
@@ -44,12 +39,43 @@ const EducationItems = ({ setEducationInfo, id }) => {
         name="universityName"
         placeholder="University name"
         onChange={handleChange}
+        value={educationItems.universityName}
       />
-      <input type="text" name="city" placeholder="City" />
-      <input type="text" name="degree" placeholder="Degree" />
-      <input type="text" name="subject" placeholder="Subject" />
-      <input type="text" name="from" placeholder="From" />
-      <input type="text" name="to" placeholder="To" />
+      <input
+        type="text"
+        name="city"
+        placeholder="City"
+        onChange={handleChange}
+        value={educationItems.city}
+      />
+      <input
+        type="text"
+        name="degree"
+        placeholder="Degree"
+        onChange={handleChange}
+        value={educationItems.degree}
+      />
+      <input
+        type="text"
+        name="subject"
+        placeholder="Subject"
+        onChange={handleChange}
+        value={educationItems.subject}
+      />
+      <input
+        type="text"
+        name="from"
+        placeholder="From"
+        onChange={handleChange}
+        value={educationItems.from}
+      />
+      <input
+        type="text"
+        name="to"
+        placeholder="To"
+        onChange={handleChange}
+        value={educationItems.to}
+      />
     </>
   );
 };
